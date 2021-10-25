342f35da3c98d1292d766a8bfe47492f3142b6e8
sangha <sangha0411@gmail.com>
2021-10-25 07:26:25 +0000

    Load Best Preprocessing

--- preprocessor.py
+++ preprocessor.py
@@ -2,7 +2,15 @@ import re
 
 class Preprocessor :
     def __init__(self, ) :
-        pass
+        ch_start_idx = int('4E00', 16)
+        ch_end_idx = int('9FFF', 16)
+        jp_start_idx = int('3040', 16)
+        jp_end_idx = int('30FF', 16)
+
+        self.ch_sub = re.compile(r'\([' + chr(ch_start_idx) + '-' + chr(ch_end_idx) + chr(jp_start_idx) + '-' + chr(jp_end_idx) + ' ]+\)')
+        self.unicode_comp1 = re.compile('[' + chr(0) + '-' + chr(31) + ']')
+        self.unicode_comp2 = re.compile('[' + chr(8191) + '-' + chr(12288) + ']')
+        self.unicode_comp3 = re.compile('[' + chr(55204) + '-' + chr(63743) + ']')
 
     def preprocess_train(self, dataset) :
         assert isinstance(dataset, dict)
@@ -43,22 +51,43 @@ class Preprocessor :
 
     def preprocess_context(self, context) :
         context = self.remove_newline(context)
-        context = self.remove_outrange(context)
+        context = self.remove_special_unicode(context)
         context = self.convert_foreign(context)
         return context
 
     def remove_newline(self, txt) :
-        txt = txt.replace('\u3000', ' ')
-        txt = txt.replace('\n', ' ')
-        txt = txt.replace('\\n', ' ')
-        txt = re.sub('\s+', ' ', txt)
+        """[summary] : remove '\n' code
+        Args:
+            txt ([str]): question and context
+        Returns:
+            [str]: text which '\n' characters is removed
+        """
+        txt = txt.replace('</br>', '')
+        txt = txt.replace(r'\n**' , '')
+        txt = txt.replace(r'\n*' , '')
+        txt = txt.replace(r'\n#' , '')
+        txt = txt.replace(r'\n' , '')
         return txt
     
-    def remove_outrange(self, txt) :
-        txt = re.sub('[힣-\uffff]', ' ', txt)
-        txt = re.sub('\s+', ' ', txt)
+    def remove_special_unicode(self, txt) :
+        """[summary] : remove special unicode (e.g '\u3000' , '\xa0' ...)
+        Args:
+            txt ([str]): question and context
+        Returns:
+            [str]: Text which speical unicode is removed 
+        """
+        txt = self.unicode_comp1.sub(' ', txt)
+        txt = self.unicode_comp2.sub(' ', txt)
+        txt = self.unicode_comp3.sub(' ', txt)
+        txt = re.sub('\s+' , ' ', txt)
         return txt
 
     def convert_foreign(self, txt) :
-        txt = re.sub('\\([\u4e00-\u9fff\u3040-\u31ff ]+\\)', '[CHN]', txt)
+        """[summary] : convert chinese word to [CHN] token, and japanese word to [JPN] token
+        Args:
+            txt ([str]): question, context and answer
+        Returns:
+            [str]: converted txt
+        """
+        txt = self.ch_sub.sub('[CHN]', txt)
         return txt

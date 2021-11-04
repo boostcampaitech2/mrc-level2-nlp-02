d94e80a09cae842c83527aca2e01aa5ffdb33257
박범진 <55076964+presto105@users.noreply.github.com>
2021-11-04 17:30:52 +0900

    Update rt_bm25.py
    bug fix

--- retriever/rt_bm25.py
+++ retriever/rt_bm25.py
@@ -262,7 +262,7 @@ class SparseRetrieval:
         pickle_name = f"BM25_retrieve_{pickle_path}.bin"
         if os.path.isfile(pickle_name) :
             with open(pickle_name, "rb") as file:
-                cqas = pickle.load(pickle_name)
+                cqas = pickle.load(file)
             print("BM25 retrieve pickle load.")
             return cqas
         else :
